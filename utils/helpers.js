0a8a53fa59d5c7e4777781b8b608bb0093c67867
DevRayHe <devrayhe@gmail.com>
2021-12-05 13:14:55 +1100

    added formate date YMD helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,4 +4,15 @@ module.exports = {
     // Format date as MM/DD/YYYY
   return date.toLocaleDateString();
   },
+
+  fromat_date_today_YMD: () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    
+    const today_YMD = yyyy + '-' + mm + '-' + dd;
+    return today_YMD;
+  }
 };
